dd6da8763dfe8beb9ed3ef51cdcf3862003d3542
JoelVRV <joelc456789@gmail.com>
2025-02-12 01:00:09 +0100

    NAV COMPONENT

--- e-commerce-website/src/components/Nav.tsx
+++ e-commerce-website/src/components/Nav.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import { Menu, Search, ShoppingCart, User } from 'lucide-react';
+
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className=''>
+        <div className="mx-auto py-1 bg-black">
+            <p className='text-white text-center text-sm md:text-base'>Sign up and get 20% off to your first order. <span className='font-semibold underline'>Sign Up</span></p>
+        </div>
+        <div className="max-w-[1240px] mx-auto flex justify-between py-5 px-5">
+            <div className='flex content-center justify-center items-center'>
+                <Menu className='mr-1' size={25}/>
+                <p className='font-black text-3xl mx-1'>React.</p>   
+            </div>
+            <div className='flex content-center justify-center items-center'>
+                <Search className="mx-1" size={25}/>
+                <ShoppingCart className="mx-1" size={25}/>
+                <User className="ml-1" size={25}/>
+            </div>
+        </div>
+    </nav>
   )
 }
 
