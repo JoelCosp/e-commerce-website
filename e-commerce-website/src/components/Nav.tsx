7fb86e5255a298ea4413e58f58e22789c03fa566
JoelVRV <joelc456789@gmail.com>
2025-02-12 02:01:10 +0100

    NAVBAR TERMINADO "APARENTEMENTE"
    - Nav responsive creado y con toggle menu btn funcional

--- e-commerce-website/src/components/Nav.tsx
+++ e-commerce-website/src/components/Nav.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useState } from "react";
 
-import { Menu, Search, ShoppingCart, User } from 'lucide-react';
+import { Menu, Search, ShoppingCart, User, ChevronDown, ShoppingBag, Tag, PackageCheck, Building2  } from 'lucide-react';
 
 const Nav = () => {
+
+    const [nav, setNav] = useState(false);
+
+    function toggleNav()
+    {
+        setNav(!nav);
+    }
+
   return (
     <nav className=''>
         <div className="mx-auto py-1 bg-black">
@@ -10,14 +19,34 @@ const Nav = () => {
         </div>
         <div className="max-w-[1240px] mx-auto flex justify-between py-5 px-5">
             <div className='flex content-center justify-center items-center'>
-                <Menu className='mr-1' size={25}/>
+                <button onClick={toggleNav}><Menu className='mr-1 block sm:hidden' size={25}/></button>
                 <p className='font-black text-3xl mx-1'>React.</p>   
+            </div>
+            <div className='flex content-center justify-center items-center hidden sm:block'>
+                <ul className='flex'>
+                    <li className='mx-5 flex font-semibold content-center justify-center items-center hover:cursor-pointer'>Shop <ChevronDown color="#000000" size={20}/></li>
+                    <li className='mx-5 font-semibold hover:cursor-pointer'>On Sale</li>
+                    <li className='mx-5 font-semibold hover:cursor-pointer'>New Arrivals</li>
+                    <li className='mx-5 font-semibold hover:cursor-pointer'>Brands</li>
+                </ul>
             </div>
             <div className='flex content-center justify-center items-center'>
                 <Search className="mx-1" size={25}/>
                 <ShoppingCart className="mx-1" size={25}/>
                 <User className="ml-1" size={25}/>
             </div>
+        </div>
+        {/* LATERAL MENU */}
+        <div className={nav ? 'block absolute top-0 bg-white left-0 w-[60%] h-full px-6 pt-[40px] shadow-lg block sm:hidden' : 'hidden'}>
+            <div className="w-full flex justify-end">
+                <button onClick={toggleNav}><Menu className='block sm:hidden' size={25}/></button>
+            </div>
+            <ul>
+                <li className='flex content-center items-center py-6 border-b border-b-gray-300'><ShoppingBag className="mr-2" size={15}/> Shop</li>
+                <li className='flex content-center items-center py-6 border-b border-b-gray-300'><Tag className="mr-2" size={15}/> On Sale</li>
+                <li className='flex content-center items-center py-6 border-b border-b-gray-300'><PackageCheck className="mr-2" size={15}/> New Arrivals</li>
+                <li className='flex content-center items-center py-6 border-b border-b-gray-300'><Building2 className="mr-2" size={15}/> Brands</li>
+            </ul>
         </div>
     </nav>
   )
